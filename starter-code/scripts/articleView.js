92dd47c1ee59f0f9905e7aa5fe6d4eb659ab063e
RecursiveIteration <James.C.Woodson@gmail.com>
2017-07-14 16:41:09 -0700

    started on articleView.create and moved on to remove blockers

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -90,7 +90,8 @@ articleView.initNewArticlePage = function() {
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
-
+  var newArticle = new Article();
+  $('#articles').empty();
 
   // TODO: Instantiate an article based on what's in the form fields:
 
